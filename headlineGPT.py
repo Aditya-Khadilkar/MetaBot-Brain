fcad75bbe9bafacee02b8579f32c8e047a4ced75
Aditya Khadilkar <35966791+Aditya-Khadilkar@users.noreply.github.com>
2021-04-18 16:14:48 +0530

    Update headlineGPT.py

--- headlineGPT.py
+++ headlineGPT.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 def createHeadline(currentInput):
-    openai.api_key = "sk-y6412Z1ugQzfXGtpeG8tzf18m1Q2IzaLHkWl3wqh"
+    openai.api_key = "API-KEY-HERE"
     start_sequence = "\nOutput:"
     restart_sequence = "\n\nInput: "
     boiler = "Headline generator\n\nInput: I want to make a fact checking chatbot that is funny. I think should be sarcastic as well.\nOutput: The following is a conversation between a Human and AI. The AI is a fact checking chatbot and is funny, sarcastic.\n\nInput: I want to make a Senko San chatbot which is meant for friendly conversations.  She is a very cute and helpful fox girl. She is Japanese by the way\nOutput: The following is a conversation between a Human and Senko San. Senko San is a chatbot for friendly conversations and is very cute, helpful, japanese fox girl.\n\nInput: Lets make a daffy duck chatbot, basically that chatbot talks like daffy duck.\nOutput:  The following is a conversation between a Human and daffy duck. daffy duck chatbot talks like daffy duck.\n\nInput: I want a lisa manobahn chatbot. she is a kpop star in the group blackpink, shes funny, clumsy and cute.\nOutput: The following is a conversation between a Human and Lisa Manobahn. Lisa Manobahn is a kpop star in the group blackpink, she is funny, clumsy and cute.\n\nInput: "
